e3e4af73142c084a5c3feec00636c9be83df920d
Nelson_AI <kcneil1200@gmail.com>
2022-12-29 23:08:15 +0100

    Add files via upload
    Excersise 3 was solved in two ways, the First part and the second, anyone that seems favourable.....

---
+++ Excersise 3/First way/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Name
+{
+        /// <summary>
+        /// Write a program and ask the user to enter the width and height of an image. Then tell if the image 
+        /// is landscape or portrait.
+        /// </summary>
+    class program{
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter width: ");
+                var width = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter height: ");
+                var height = Convert.ToInt32(Console.ReadLine());
+
+                if(width > height){
+                    Console.WriteLine("Image is a landscape");
+                }
+                else if(height > width) {
+                    Console.WriteLine("Image is a Portrait");
+                }
+                else{
+                    Console.WriteLine("INVALID INPUT");
+                }   
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("INVALID INPUT, PLEASE ENTER A NUMBER.");
+            }
+        }
+    }
+}
---
+++ Excersise 3/Second way/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Name
+{
+    public enum ImageSize{
+        landscape,
+        Portrait
+    }
+    class program{
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter width: ");
+                var width = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter height: ");
+                var height = Convert.ToInt32(Console.ReadLine());
+
+                var size = (width > height) ? ImageSize.landscape : ImageSize.Portrait;
+                Console.WriteLine("Image size is: "+ size);
+            }
+            catch (System.Exception)
+            {
+                Console.WriteLine("INVALID INPUT, PLEASE ENTER A NUMBER.");
+            }
+
+        }
+    }
+}
