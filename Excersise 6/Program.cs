0ceb5decd498f1e7324b5ed7c691f46214e69d12
Nelson_AI <kcneil1200@gmail.com>
2022-12-30 23:40:19 +0100

    Add files via upload

---
+++ Excersise 6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Name
+{
+    class program{
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Write a program and continuously ask the user to enter a number. The loop terminates when the user 
+            /// enters “ok". Calculate the sum of all the previously entered numbers and display it on the console.
+            /// </summary>
+
+            try
+            {
+                var sum = 0;
+	
+	            while (true){
+	                Console.WriteLine("Enter a number: ");
+	                var input = Console.ReadLine();
+	                
+	                if(input == "OK" || input=="ok" || input == "exit" || input=="Exit"){
+	                    break;
+	                }
+	                sum+=Convert.ToInt32(input);
+	            }
+	            Console.WriteLine("sum is: " + sum);
+            }
+            catch (System.Exception)
+            { 
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
