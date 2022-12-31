52db744508811adf59575f9657b9e63ec6766248
Nelson_AI <kcneil1200@gmail.com>
2022-12-31 23:05:17 +0100

    Add files via upload

---
+++ Excersise 7/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Name
+{
+    class program{
+        static void Main(string[] args)
+        {
+            /// <summary>
+            /// Write a program which takes a single argument from the console, computes the factorial and prints the 
+            /// value on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 
+            /// and display it as 5! = 120.
+            /// </summary>
+
+            // FIRST SOLUTION
+            Console.WriteLine("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+            if(number <= 0){
+                Console.WriteLine("Cant compute Factorail");
+            }
+            else
+            {
+                var factorial = 1;
+                for(var counterVar = 1; counterVar<=number; counterVar ++){
+                    factorial*=counterVar;
+                }
+                Console.WriteLine("{0}! = {1} ", number, factorial);
+            } 
+
+            // SECOND SOLUTION
+            Console.WriteLine("Enter a number: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+            if(input <= 0){
+                Console.WriteLine("Cant compute Factorial");
+            }
+            else
+            {   
+                var factorial = 1;
+                for(var i= 1; i<=input; i++){
+                    factorial = factorial*i;
+                }
+                Console.WriteLine("Factorial of number is: " + factorial);
+            } 
+        }
+    }
+}
---
+++ Excersise 7/tempCodeRunnerFile.cs
@@ -0,0 +1,13 @@
+            // Console.WriteLine("Enter a number: ");
+            // var number = Convert.ToInt32(Console.ReadLine());
+            // if(number <= 0){
+            //     Console.WriteLine("Cant compute Factorail");
+            // }
+            // else
+            // {
+            //     var factorial = 1;
+            //     for(var counterVar = 1; counterVar<=number; counterVar ++){
+            //         factorial*=counterVar;
+            //     }
+            //     Console.WriteLine("{0}! = {1} ", number, factorial);
+            // } 
